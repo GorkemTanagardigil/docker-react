772d8c1677f8cfe647f4053408837082589cd1be
Görkem Tanağardıgil <gorkem.tanagardigil@rightyon.com>
2022-12-25 17:17:14 +0300

    changed app text

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hi Görkem
+          Hi Görkem This from feature
         </p>
         <a
           className="App-link"
